6fa2cecf31bd759f1bdba008d80efc8fc0566a48
Linn <LINNENT.23@ichat.sp.edu.sg>
2024-10-21 09:14:29 +0800

    Calculating bmi with if else statement

--- bmi.py
+++ bmi.py
@@ -1,6 +1,18 @@
 def calculate_bmi(height,weight):
-    print("Height = " , height)
-    print("Weight = " , weight)
+    print("Height = " + height)
+    print("Weight = " + weight)
 
 calculate_bmi(weight="57", height="1.73")
-calculate_bmi(1.73,57)
+#calculate_bmi(1.73,57)
+
+def calculate_bmi_2(height,weight):
+    bmi = weight/(height*height)
+    return bmi
+
+bmi_value = calculate_bmi_2(1.73,57)
+if bmi_value < 18.5:
+    print("Under weight")
+elif bmi_value<=25.0:
+    print("Normal Weight")
+else:
+    print("Over Weight")
