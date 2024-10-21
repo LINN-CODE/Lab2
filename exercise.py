9b8fd750ae1a1e0b0123f4bfb50d10d43777c287
Linn <LINNENT.23@ichat.sp.edu.sg>
2024-10-21 10:02:43 +0800

    exercise 3 and exercise 4

--- exercise.py
+++ exercise.py
@@ -2,6 +2,8 @@ def main():
     print("ET0735 (DevOps for AIot) - Lab2 - Introductin to python")
     display_main_menu()
     num_list = get_user_input()
+    average = calculate_average(num_list)
+    max_min = large_small(num_list)
 
 def display_main_menu():
     print("display_main_menu")
@@ -14,8 +16,14 @@ def get_user_input():
         print(x)
         list_array.append(float(x))
     #float_array = [float(string) for string in string_array]
-    print(list_array)
+    return list_array
 
+def calculate_average(numlist):
+    print("The average value is " , sum(numlist)/len(numlist))
+
+def large_small(numlist):
+    print("The maximum value is ", max(numlist))
+    print("The minimum value is ", min(numlist))
 
 if __name__ == "__main__":
     main()
