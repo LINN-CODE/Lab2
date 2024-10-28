4d449266ca58c2a5d0fdf9fd02115dd925e19a7a
Linn <LINNENT.23@ichat.sp.edu.sg>
2024-10-28 09:11:00 +0800

    Adding function

--- bmi.py
+++ bmi.py
@@ -7,14 +7,16 @@ calculate_bmi(weight="57", height="1.73")
 
 def calculate_bmi_2(height,weight):
     bmi = weight/(height*height)
-    return bmi
-
+    if bmi < 18.5:
+        print("Under weight")
+        return -1
+    elif bmi_value<=25.0:
+        print("Normal Weight")
+        return 0
+    else:
+        print("Over Weight")
+        return 1
+    
 bmi_value = calculate_bmi_2(1.73,57)
-if bmi_value < 18.5:
-    print("Under weight")
-elif bmi_value<=25.0:
-    print("Normal Weight")
-else:
-    print("Over Weight")
 
 
